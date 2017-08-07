47c792a5f3018055e61b93d1905e38183c1b9aee
GoldenFox <Glo0975@yandex.ru>
2017-08-07 17:43:18 +0400

    Исправлена ошибка при добавлении колонки в дизайнмоде

---
+++ pl_Orca_Ext_Tests/TreeGrid_AddColumn/unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, orca_scene2d, Forms, Controls, Graphics, Dialogs;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    D2Scene1: TD2Scene;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
