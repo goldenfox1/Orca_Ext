96327801b6092c27bb01aebff8fc12206f6c03c7
GoldenFox1 <glo0975@yandex.ru>
2019-09-16 10:48:00 +0400

    Промежуточная

---
+++ pl_Orca_Ext_Tests/Тест/unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, orca_scene2d;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    D2Scene1: TD2Scene;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
